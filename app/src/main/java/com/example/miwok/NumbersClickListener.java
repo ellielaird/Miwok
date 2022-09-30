f4354ce0fe1b66d43d9156134f20560a7e2c53dd
Ellie Laird <ellielaird@icloud.com>
2022-09-30 10:57:33 -0500

    created intents; working on onclick method

---
+++ app/src/main/java/com/example/miwok/NumbersClickListener.java
@@ -0,0 +1,13 @@
+package com.example.miwok;
+
+
+import android.content.Intent;
+import android.view.View;
+
+public class NumbersClickListener implements View.OnClickListener {
+    @Override
+    public void onClick(View view) {
+
+    }
+}
+
