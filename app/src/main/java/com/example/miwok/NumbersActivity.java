52d15dee41385d9ac98c1cd00ad3cd80d5f0a4e9
Ellie Laird <ellielaird@icloud.com>
2022-10-04 12:47:57 -0500

    added numbers list and began working on the xml file that defines each section of the numbers list

--- app/src/main/java/com/example/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/miwok/NumbersActivity.java
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -13,7 +19,29 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_numbers);
 
 
-        String[] englishNumbers = new String[]{"One","Two","Three","Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"};
+
+
+        ArrayList<String> englishNumbers = new ArrayList<String>();
+        englishNumbers.add(0, "One");
+        englishNumbers.add(1, "Two");
+        englishNumbers.add(2, "Three");
+        englishNumbers.add(3, "Four");
+        englishNumbers.add(4, "Five");
+        englishNumbers.add(5, "Six");
+        englishNumbers.add(6, "Seven");
+        englishNumbers.add(7, "Eight");
+        englishNumbers.add(8, "Nine");
+        englishNumbers.add(9, "Ten");
+
+
+
+
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, englishNumbers);
+
+        ListView listView = (ListView)findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
 
 
     }
